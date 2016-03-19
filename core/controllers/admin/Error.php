bfd7cd6c4b3579f3455678644c64606a6e627269
D-LUSiON <D-LUSiON@AsusG750JZ>
2016-03-19 13:10:16 +0200

    NEW: If image is not found, redirects to img/no_image.png in "Base" theme;

--- core/controllers/admin/Error.php
+++ core/controllers/admin/Error.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace Controllers;
 
 class Error extends BaseController {
@@ -6,9 +7,18 @@ class Error extends BaseController {
     function __construct() {
         
     }
-    
-    public function index($errorText = ''){
+
+    public function index($errorText = '') {
         echo $errorText;
+    }
+
+    public function noImage() {
+        $name = realpath(THEMES_DIR . 'base/img/no_photo.png');
+        
+        header('Content-Type: image/png');
+        header('Content-Length: ' . filesize($name));
+
+        readfile($name);
     }
 
 }
